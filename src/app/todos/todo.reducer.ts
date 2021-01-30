da9efd695c9988cf08928ce6e56ea5af5fb38cdc
Edwin Quintero <edwi02@gmail.com>
2021-01-30 10:45:46 -0500

    NGRX instalado

---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,15 @@
+import { createReducer, on } from '@ngrx/store';
+import * as actions from './todo.actions';
+import { Todo } from './models/todo.model';
+
+export const initialState: Todo[] = [];
+
+const _todoReducer = createReducer(
+    initialState,
+    // tslint:disable-next-line:max-line-length
+    on(actions.crear, (state, { texto }) => [...state, new Todo( texto )] ) // Extraemos cada uno de los ítems y los regresamos de manera independiente
+);
+
+export function todoReducer( state, action ): any {
+    return _todoReducer( state, action );
+}
