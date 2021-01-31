e6d9bcbd0a76191fdfe181ee2456750c246ddac0
Edwin Quintero <edwi02@gmail.com>
2021-01-30 19:04:02 -0500

    Cambio en el state de Filtros

--- src/app/filter/filter.actions.ts
+++ src/app/filter/filter.actions.ts
@@ -1,6 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 
-export type filtrosValidos= 'todos' | 'completados' | 'pendientes;'
+export type filtrosValidos= 'todos' | 'completados' | 'pendientes';
 
 export const setFilter = createAction(
     '[Filter] Set Filter',
--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,8 @@
+import { Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
+
+import { AppState } from '../../app.reducer';
+import * as actions from '../../filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  filtroActual: actions.filtrosValidos = 'todos';
+  lstFiltros: actions.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+
+  constructor( private store: Store<AppState>) { }
 
   ngOnInit(): void {
+
+    this.store.select('filtro').subscribe( filtro => this.filtroActual = filtro );
+
+  }
+
+  cambiarFiltro( filtro: actions.filtrosValidos ): void {
+    console.log(filtro);
+    this.store.dispatch( actions.setFilter({ filtro }) );
   }
 
 }
